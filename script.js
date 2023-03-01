a4fa4f8feb60cc6b72ae0f709264fbb3b9f818a4
Alumno <Alumno@alumno.com>
2023-03-01 14:14:20 +0100

    3º versión, funcionalidad de salir pulsando el escape  + otra forma de hacer el event listener

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //Selección de los elementos del DOM
-const SHOWBUTTONS = document.querySelectorAll(".show-modal");
+//const SHOWBUTTONS = document.querySelectorAll(".show-modal");
+const BTNSHOWMODAL = document.querySelector("#botones");
 const CLOSEBUTTON = document.querySelector(".close-modal");
 const OVERLAY = document.querySelector(".overlay");
 const MODAL = document.querySelector(".modal");
 
-//Comprobar
-console.log(SHOWBUTTONS, CLOSEBUTTON, OVERLAY, MODAL);
-
 //Funciones
-const showModal = function () {
-  MODAL.classList.remove("hidden");
-  OVERLAY.classList.remove("hidden");
+const showModal = function (e) {
+  console.log(e, e.target);
+  if (e.target.classList.contains("show-modal")) {
+    MODAL.classList.remove("hidden");
+    OVERLAY.classList.remove("hidden");
+  }
 };
 
 const closeModal = function () {
@@ -19,6 +20,13 @@ const closeModal = function () {
 };
 
 //Definir eventos
-SHOWBUTTONS.forEach((element) => element.addEventListener("click", showModal));
+BTNSHOWMODAL.addEventListener("click", showModal);
 CLOSEBUTTON.addEventListener("click", closeModal);
 OVERLAY.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !MODAL.classList.contains("hidden")) {
+    closeModal();
+  }
+  e.stopPropagation();
+});
